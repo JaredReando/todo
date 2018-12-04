086f315503687f5bcc03457c52d0198519137060
Student <example@example.com>
2018-12-04 11:40:58 -0800

    Updates

---
+++ lib/task.rb
@@ -0,0 +1,35 @@
+require'pry'
+
+class Task
+  attr_reader(:description, :list_id)
+
+  def initialize(attributes)
+    @description = attributes[:description]
+    @list_id = attributes[:list_id]
+  end
+
+  def self.all
+    returned_tasks = DB.exec("SELECT * FROM tasks;")
+    tasks = []
+    returned_tasks.each() do |task|
+      description = task["description"]
+      list_id = task["list_id"].to_i
+      tasks.push(Task.new({:description => description, :list_id => list_id}))
+    end
+    tasks
+  end
+
+  def ==(other)
+    @description == other.description && @list_id == other.list_id
+  end
+
+  def save
+    DB.exec("INSERT INTO tasks (description, list_id) VALUES ('#{@description}', #{@list_id});")
+  end
+
+  def show_tasks(list_id)
+    tasks = DB.exec("SELECT description FROM tasks WHERE list_id = #{list_id}")
+    tasks
+  end
+
+end
